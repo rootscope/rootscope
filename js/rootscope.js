a116e1006e39a1da2a6cb4351a34927597e5774b
rootscope <datrootscope@gmail.com>
2015-01-08 19:58:11 -0700

    bg shows

--- js/rootscope.js
+++ js/rootscope.js
@@ -1,4 +1,28 @@
-angular.module("rootApp", [])
-.controller("rootAppCtrl", function($scope){
+var root = angular.module("rootApp", [])
 
+root.directive("scrollPos", function($window){
+  return {
+    scope: {
+      scroll: '=scrollPos'
+    },
+    link: function(scope, element, attrs) {
+      var windowEl = angular.element($window);
+      var handler = function(){
+        scope.scroll = windowEl.scrollTop();
+      }
+      windowEl.on('scroll', scope.$apply.bind(scope, handler));
+      handler();
+    }
+  };
 });
+
+root.controller("rootAppCtrl", function($scope, $window){
+  $scope.scroll = 0;
+});
+
+/*
+$(window).scroll(function(e) {
+  opacity = ($(window).scrollTop() / 170);
+  $(".blur").css("opacity", opacity);
+});
+*/
