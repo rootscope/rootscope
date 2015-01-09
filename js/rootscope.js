6add10e55d394e52915a9cf13f3ea10955fba1ec
rootscope <datrootscope@gmail.com>
2015-01-08 20:57:54 -0700

    complete scrollPosition directive

--- js/rootscope.js
+++ js/rootscope.js
@@ -1,28 +1,20 @@
-var root = angular.module("rootApp", [])
+var app = angular.module("rootApp", []);
 
-root.directive("scrollPos", function($window){
+app.directive('scrollPosition', function($window) {
   return {
     scope: {
-      scroll: '=scrollPos'
+      scroll: '=scrollPosition'
     },
     link: function(scope, element, attrs) {
-      var windowEl = angular.element($window);
-      var handler = function(){
-        scope.scroll = windowEl.scrollTop();
+      var handler = function() {
+        scope.scroll = $window.scrollY;
       }
-      windowEl.on('scroll', scope.$apply.bind(scope, handler));
+      angular.element($window).on('scroll', scope.$apply.bind(scope, handler));
       handler();
     }
   };
 });
 
-root.controller("rootAppCtrl", function($scope, $window){
+app.controller("rootAppCtrl", function($scope){
   $scope.scroll = 0;
 });
-
-/*
-$(window).scroll(function(e) {
-  opacity = ($(window).scrollTop() / 170);
-  $(".blur").css("opacity", opacity);
-});
-*/
