c3cd64b56fd5548b3339135b8a0ba436e7f1a93c
rootscope <datrootscope@gmail.com>
2015-01-21 18:21:58 -0700

    sp

--- js/rootscope.js
+++ js/rootscope.js
@@ -7,16 +7,9 @@ app.directive('scrollBlur', function($window){
     },
     link: function(scope, element, attrs) {
       var handler = function() {
-        scope.opacity = $window.scrollY / 420;
-        console.log(scope.opacity);
+        scope.opacity = $window.scrollY / 220;
       }
       angular.element($window).on('scroll', scope.$apply.bind(scope, handler));
-
-      if(window.scrollY < 1){
-        handler();
-      }else{
-        console.error('wtf');
-      }
     }
   };
 });
@@ -30,8 +23,34 @@ app.directive('scrollPosition', function($window) {
       var handler = function() {
         scope.scroll = $window.scrollY;
       }
+      console.log('test2');
+      if(scope.scroll < 497){
+        angular.element($window).on('scroll', scope.$apply.bind(scope, handler));
+        handler();
+      }
+      else{
+        console.log('test');
+      }
+    }
+  };
+});
+
+app.directive('splashToNav', function($window){
+  return{
+    scope: {
+      navbar: '=splashToNav'
+    },
+    link: function(scope, element, attrs){
+      var handler = function(){
+        scope.navbar = true;
+      }
+      if(scope.scrollY < 1){
+        console.error('navbar = true');
+        scope.navbar = true;
+      }else{
+        console.error('wtf');
+      }
       angular.element($window).on('scroll', scope.$apply.bind(scope, handler));
-      handler();
     }
   };
 });
@@ -40,6 +59,7 @@ app.controller("rootAppCtrl", function($scope, $window){
   $scope.scroll = 0;
   $scope.opacity = 0;
   $scope.margin = 0;
+  $scope.navbar = false;
 });
 
 var ui = (function($){
@@ -51,7 +71,7 @@ var ui = (function($){
   var laxSplash = function(o){
     //$e.splash_bg.css('margin-top', -0.07*o + 'px');
     $e.splash_title.css('margin-top', -0.84*o + 'px');
-    //$e.splash_title.css('margin-left', -0.19*o + 'px');
+    $e.splash_title.css('margin-left', -0.19*o + 'px');
   };
   var init = function(){
     $e.window.scroll(function(e){
